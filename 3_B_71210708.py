d1827a3986110a803856e38f9b37a690d6b0f10f
josephineangie <93273072+josephineangie@users.noreply.github.com>
2021-11-11 22:56:46 +0700

    3_B_71210708

---
+++ 3_B_71210708.py
@@ -0,0 +1,7 @@
+nilai1 = int(input ("Nilai 1 : "))
+nilai2 = int(input ("Nilai 2 : "))
+nilai3 = int(input ("Nilai 3 : "))
+
+rata = float(nilai1 + nilai2 + nilai3) / 3
+
+print ("Rata-rata dari", nilai1, "+", nilai2, "+" ,nilai3, "adalah", rata)
